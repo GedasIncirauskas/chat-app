5d9af8770f0e400d4b61249fb2d9f0b5058d70a1
GedasIncirauskas <gedas.inc@gmail.com>
2023-08-07 17:06:25 +0300

    created button component

---
+++ src/components/ui/buttons/Button.tsx
@@ -0,0 +1,53 @@
+import { fn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import { ImSpinner } from "react-icons/im";
+import { ButtonHTMLAttributes, FC } from "react";
+
+const buttonVariants = cva(
+  "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
+  {
+    variants: {
+      variant: {
+        default: "bg-slate-900 text-white hover:bg-slate-800",
+        ghost: "bg-transparent hover:text-slate-900 hover:bg-slate-200",
+      },
+      size: {
+        default: "h-9 py-2 px-4",
+        sm: "h-9 px-2",
+        large: "h-14 px-8",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+      variant: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+const Button: FC<ButtonProps> = ({
+  className,
+  children,
+  variant,
+  isLoading,
+  size,
+  ...props
+}) => {
+  return (
+    <button
+      className={fn(buttonVariants({ variant, size, className }))}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? <ImSpinner className="mr-2 h-4 w-4 animate-spin" /> : null}
+      {children}
+    </button>
+  );
+};
+
+export default Button;

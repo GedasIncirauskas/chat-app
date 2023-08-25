c9506188a88c31b50950a9d4fb22d02ecf842e63
GedasIncirauskas <gedas.inc@gmail.com>
2023-08-25 13:54:03 +0300

    arrow function added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,11 +5,11 @@ export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
 
-export function toPusherKey(key: string) {
+export const toPusherKey = (key: string) => {
   return key.replace(/:/g, "__");
-}
+};
 
-export function chatHrefConstructor(id1: string, id2: string) {
+export const chatHrefConstructor = (id1: string, id2: string) => {
   const sortedIds = [id1, id2].sort();
   return `${sortedIds[0]}--${sortedIds[1]}`;
-}
+};
